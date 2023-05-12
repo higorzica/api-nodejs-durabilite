0ebd1f5984839268e66e862810858ada14c2653a
Higor Zica <higorbhe@gmail.com>
2023-05-12 14:44:23 +0200

    alteracao na connection sequelize

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -4,7 +4,11 @@ const sequelize_1 = require("sequelize");
 require("dotenv/config");
 const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME || 'durabilite', process.env.DB_USER || 'root', process.env.DB_PASSWORD || 'higor123', {
     host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql'
+    dialect: process.env.DB_DIALECT || 'mysql'
 });
-console.log(process.env.DB_NAME);
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+});
 exports.default = sequelize;
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,28 +4,28 @@ dotenv.config();
 
 const config = {
     "desenvolvedor": {
-        "DB_HOST" : process.env.DB_HOST,
-        "DB_USER" : process.env.DB_USER,
-        "DB_PASSWORD" : process.env.DB_PASSWORD,
-        "DB_NAME" : process.env.DB_NAME,
-        "DB_PORT" : process.env.DB_PORT,
-        "DB_DIALECT": process.env.DB_DIALECT
+        "host" : process.env.DB_HOST,
+        "user" : process.env.DB_USER,
+        "password" : process.env.DB_PASSWORD,
+        "dbname" : process.env.DB_NAME,
+        "dbport" : process.env.DB_PORT,
+        "dialect": process.env.DB_DIALECT
     },
     "teste": {
-        "DB_HOST" : process.env.DB_HOST,
-        "DB_USER" : process.env.DB_USER,
-        "DB_PASSWORD" : process.env.DB_PASSWORD,
-        "DB_NAME" : process.env.DB_NAME,
-        "DB_PORT" : process.env.DB_PORT,
-        "DB_DIALECT": process.env.DB_DIALECT
+        "host" : process.env.DB_HOST,
+        "user" : process.env.DB_USER,
+        "password" : process.env.DB_PASSWORD,
+        "dbname" : process.env.DB_NAME,
+        "dbport" : process.env.DB_PORT,
+        "dialect": process.env.DB_DIALECT
     },
     "producao": {
-        "DB_HOST" : process.env.DB_HOST,
-        "DB_USER" : process.env.DB_USER,
-        "DB_PASSWORD" : process.env.DB_PASSWORD,
-        "DB_NAME" : process.env.DB_NAME,
-        "DB_PORT" : process.env.DB_PORT,
-        "DB_DIALECT": process.env.DB_DIALECT
+        "host" : process.env.DB_HOST,
+        "user" : process.env.DB_USER,
+        "password" : process.env.DB_PASSWORD,
+        "dbname" : process.env.DB_NAME,
+        "dbport" : process.env.DB_PORT,
+        "dialect": process.env.DB_DIALECT
     }
 }
 
